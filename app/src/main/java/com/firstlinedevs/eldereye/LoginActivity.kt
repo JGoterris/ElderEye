b552c8aef8802043b3a82f273b759afed3875475
Jesús Goterris <jgoterris24@gmail.com>
2024-03-06 16:54:28 +0100

    Inicio sesión completo

--- app/src/main/java/com/firstlinedevs/eldereye/LoginActivity.kt
+++ app/src/main/java/com/firstlinedevs/eldereye/LoginActivity.kt
@@ -1,11 +1,58 @@
 package com.firstlinedevs.eldereye
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import com.firstlinedevs.eldereye.ElderEyeApplication.Companion.prefs
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initUI()
+        initListeners()
+    }
+
+    private fun initUI(){
+        if(!prefs.getName().equals("")){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun initListeners(){
+        // Iniciar sesión
+        val btnLogin = findViewById<AppCompatButton>(R.id.btnLogin)
+        val etUser = findViewById<EditText>(R.id.etUser)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        val errorLoginDialog = AlertDialog.Builder(this)
+        errorLoginDialog.setTitle("Credenciales incorrectas")
+        errorLoginDialog.setMessage("Ingrese las credenciales correctas, podrá encontrarlas abajo a la derecha")
+        errorLoginDialog.setPositiveButton(android.R.string.yes) { dialog, which ->}
+
+        btnLogin.setOnClickListener {
+            if(etUser.text.toString().equals("user") && etPassword.text.toString().equals("123456")){
+                prefs.saveName("user")
+                startActivity(Intent(this, MainActivity::class.java))
+            } else{
+                errorLoginDialog.show()
+            }
+        }
+
+        // Botón registro
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
+
+        val registerClickedDialog = AlertDialog.Builder(this)
+        registerClickedDialog.setTitle("No implementado")
+        registerClickedDialog.setMessage("Debido a que esta es una aplicación prototipo, esta opción no está disponible")
+        registerClickedDialog.setPositiveButton(android.R.string.yes) { dialog, which ->}
+
+        tvRegister.setOnClickListener {
+            registerClickedDialog.show()
+        }
     }
 }
--- app/src/main/java/com/firstlinedevs/eldereye/MainActivity.kt
+++ app/src/main/java/com/firstlinedevs/eldereye/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firstlinedevs.eldereye.ElderEyeApplication.Companion.prefs
 import com.firstlinedevs.eldereye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +32,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+
     }
 }
