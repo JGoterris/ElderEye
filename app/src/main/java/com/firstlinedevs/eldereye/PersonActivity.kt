6c65ffc07141430084b2575bd3713235346c7abb
Jesús Goterris <jgoterris24@gmail.com>
2024-03-13 19:42:26 +0100

    Person Activity hecha hasta la redireccion del maps

--- app/src/main/java/com/firstlinedevs/eldereye/PersonActivity.kt
+++ app/src/main/java/com/firstlinedevs/eldereye/PersonActivity.kt
@@ -1,9 +1,11 @@
 package com.firstlinedevs.eldereye
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class PersonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,16 @@ class PersonActivity : AppCompatActivity() {
     }
 
     private fun initListeners(){
+        val btnLlamar = findViewById<Button>(R.id.btnLlamar)
+        btnLlamar.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789")))
+        }
+
+        val btnIr = findViewById<Button>(R.id.btnIr)
+        btnIr.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/2NnLQX4N4koXCL578")))
+        }
+
         val opPCamera = findViewById<ImageView>(R.id.opPCamera)
         val cameraIntent = Intent(this,CameraActivity::class.java)
         cameraIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
